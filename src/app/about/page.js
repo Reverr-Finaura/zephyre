0b6340a80b75288a8deb61fe944848ffd08e867c
Ashutosh Kewat <109691178+Ashu6200@users.noreply.github.com>
2024-03-14 19:33:02 +0530

    everything is completed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -35,7 +35,7 @@ const page = () => {
               </div>
               <div className={style.aboutHeroRight}>
                 <h5>ABOUT</h5>
-                <p className={style.title}>Zephyer Capital Ventures</p>
+                <p className={style.title}>Zephyer</p>
                 <p className={style.info}>
                   Zephyer, a unit under D Square Ventures Pvt Ltd, is a premier
                   investment management unit specializing in serving the unique
