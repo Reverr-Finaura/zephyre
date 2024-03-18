45be13e09bd2d3e7699171b90045bfeb17f4ca55
Ashutosh Kewat <109691178+Ashu6200@users.noreply.github.com>
2024-03-18 17:17:24 +0530

    TC

--- src/app/terms-&-conditions/page.js
+++ src/app/terms-&-conditions/page.js
@@ -10,8 +10,8 @@ const page = () => {
       <div className={styles.hero}>
         <Navbar2 />
         <div className={styles.blurspot}></div>
-        <div className={styles.blurspot2}></div>
         <div className={styles.blurspot3}></div>
+        <div className={styles.blurspot2}></div>
         <div className={style.serviceWrapper}>
           <div className={style.serviceContaienr}>
             <h5>PAGE </h5>
@@ -19,36 +19,36 @@ const page = () => {
             <div className={style.serviceContentArea}>
               <div className={style.servicecontentInfo}>
                 <p>
-                  By accessing and placing an order with Collosal, you confirm
+                  By accessing and placing an order with Zephyer, you confirm
                   that you are in agreement with and bound by the terms of
                   service contained in the Terms & Conditions outlined below.
                   These terms apply to the entire website and any email or other
-                  type of communication between you and Collosal.
+                  type of communication between you and Zephyer.
                   <br />
-                  Under no circumstances shall Collosal team be liable for any
+                  Under no circumstances shall Zephyer team be liable for any
                   direct, indirect, special, incidental or consequential
                   damages, including, but not limited to, loss of data or
                   profit, arising out of the use, or the inability to use, the
-                  materials on this site, even if Collosal team or an authorized
+                  materials on this site, even if Zephyer team or an authorized
                   representative has been advised of the possibility of such
                   damages. If your use of materials from this site results in
                   the need for servicing, repair or correction of equipment or
                   data, you assume any costs thereof.
                   <br />
-                  Collosal will not be responsible for any outcome that may
-                  occur during the course of usage of our resources. We reserve
-                  the rights to change prices and revise the resources usage
-                  policy in any moment.
+                  Zephyer will not be responsible for any outcome that may occur
+                  during the course of usage of our resources. We reserve the
+                  rights to change prices and revise the resources usage policy
+                  in any moment.
                 </p>
                 <h4>License</h4>
                 <p>
-                  Collosal grants you a revocable, non-exclusive, non-
+                  Zephyer grants you a revocable, non-exclusive, non-
                   transferable, limited license to download, install and use the
                   website strictly in accordance with the terms of this
                   Agreement.
                   <br />
                   These Terms & Conditions are a contract between you and
-                  Collosal ("we," "our," or "us") grants you a revocable,
+                  Zephyer ("we," "our," or "us") grants you a revocable,
                   non-exclusive, non- transferable, limited license to download,
                   install and use the website strictly in accordance with the
                   terms of this Agreement.
